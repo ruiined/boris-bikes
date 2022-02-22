d88722ef684bfb22b14061c3c091b23adc0d4fd5
Patryk Okpara <patrykokpara@hotmail.com>
2022-02-22 15:54:41 +0000

    Create bike class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,7 @@ class DockingStation
     def release_bike
 
     end
+
 end
 
 docking_station = DockingStation.new
---
+++ spec/bike_spec.rb
@@ -0,0 +1,11 @@
+require 'bike'
+describe Bike do
+  it 'working?' do
+  end
+end
+describe Bike(bike) do
+  it 'bike.respond_to' do
+  bike.working? == true
+  end   
+end
+  
